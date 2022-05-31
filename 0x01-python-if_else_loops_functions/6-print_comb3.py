e033f7106387fd8749211abaf5d337e7dad884cc
Mobius90 <mikiyasdaniel1@gmail.com>
2022-05-31 19:33:04 +0300

    loops and functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 8):
     for j in range(i + 1, 10):
         print("{0}{1}".format(i, j), end=', ')
-print("{0}{1}".format(i + 1, i + 2), end='')
+print("{0}{1}\n".format(i + 1, i + 2), end='')
