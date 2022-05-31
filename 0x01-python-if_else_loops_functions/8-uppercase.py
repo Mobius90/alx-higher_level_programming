147c5e2ee7f02ac0ab3c3b561978b26b43e41f7f
Mobius90 <mikiyasdaniel1@gmail.com>
2022-05-31 19:36:41 +0300

    loops and functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
-failed
 #!/usr/bin/python3
-from curses.ascii import isdigit
-
-
 def uppercase(str):
     for i in str:
-        if isdigit(i) ==True:
-            print("{0}".format(i), end='')
         if ord(i) > 96 and ord(i) < 123:
             i = ord(i) - 32
             print("{:c}".format(i), end='')
-uppercase("best 98");
+        else:
+            print("{0}".format(i), end='')
