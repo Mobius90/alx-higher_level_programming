c81abffa75c994c2fa20f715ce7c330d8db8be3c
Mobius90 <mikiyasdaniel1@gmail.com>
2022-05-31 15:04:55 +0300

    failed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
+failed
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
         print(f"0{i:d},", end=' ')
-    else :
+    else:
         print(f"{i:d},", end=' ')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,13 @@
+failed
 #!/usr/bin/python3
+from curses.ascii import isdigit
+
+
 def uppercase(str):
     for i in str:
+        if isdigit(i) ==True:
+            print("{0}".format(i), end='')
         if ord(i) > 96 and ord(i) < 123:
             i = ord(i) - 32
-            print(f"{chr(i)}", end='')
+            print("{:c}".format(i), end='')
+uppercase("best 98");
