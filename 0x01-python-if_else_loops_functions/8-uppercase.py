7b6a3615473a16eaf8a0673cf33a1821876905d7
Mobius90 <mikiyasdaniel1@gmail.com>
2022-05-31 12:54:01 +0300

    loops and func

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            i = ord(i) - 32
+            print(f"{chr(i)}", end='')
---
+++ test.py
@@ -0,0 +1,4 @@
+number = 5
+i = 101
+if i > 5:
+    print("Last digit of {0} is {1} and {0} is greater that 5\n".format(number, i))
