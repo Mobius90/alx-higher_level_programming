a2cab72d235ecd1e7795910aed503751a0cddae1
Mobius90 <mikiyasdaniel1@gmail.com>
2022-05-31 13:33:10 +0300

    loops and functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print(f"0{i:d},", end=' ')
+        print(f"0{0},".format(i), end=' ')
     else:
-        print(f"{i:d},", end=' ')
+        print(f"{0},".format(i), end=' ')
