cfde82d4de77c6c6650582ab3f1bb6e73fa1001d
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-08 16:28:21 +0300

    more data structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for i, j in sorted(a_dictionary.items()):
-        print("{0}: {1}".format(i, j))
-        
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
