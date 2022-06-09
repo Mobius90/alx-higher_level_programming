1e5f36165025c52ef728694a28d11a8719219e35
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-09 13:39:56 +0300

    more data structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    nlist = {'X': 10, 'VII': 7, 'IX': 9, 'LXXXVII': 87, 'DCCVII': 707}
+    nlist = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     a = 0
-    if not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
-    for i in nlist.keys():
-        if i == roman_string:
-            a = nlist[i]
+    for i in range(len(roman_string)):
+        if i > 0 and nlist[roman_string[i]] > nlist[roman_string[i - 1]]:
+            a += nlist[roman_string[i]] - 2 * nlist[roman_string[i - 1]]
+        else:
+            a += nlist[roman_string[i]]
     return a
