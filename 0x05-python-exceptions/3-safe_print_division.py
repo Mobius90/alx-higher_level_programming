44e02a430e77297c20c64c106bf491e564d4bb08
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-20 15:26:51 +0300

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,4 +7,4 @@ def safe_print_division(a, b):
         result = None
         return None
     finally:
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {0}".format(result))
