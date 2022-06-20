ad6bd8fb1ef381dfd9ab233b23152dd0a26ca03d
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-20 13:12:13 +0300

    exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        for i in value:
+            print("{:d}".format(i), end="")
+        print("")
+        return True
+    except:
+        return False
