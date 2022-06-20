3b52cc434d5b074df9b8de9367bce370834ec19f
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-20 13:37:20 +0300

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try: 
             print("{:d}".format(my_list[i]), end="")
             y = y + 1
-        except Exception (ValueError, TypeError):
+        except (ValueError, TypeError):
             pass
     print("")
     return y
