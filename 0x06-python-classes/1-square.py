531e0924b89241867cd09612ad98cfa90856cedc
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-21 21:59:21 +0300

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a module that defines a Square class and initialise its size
+"""
+
+
+class Square:
+    """
+    a class with private instance attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
