55d0b7bb92ecc8e290dc5627d0abbff016a81a03
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-27 15:30:12 +0300

    more classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,6 +17,21 @@ class Rectangle:
         self.width = width
         Rectangle.number_of_instances += 1
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return Rectangle(size, size)
+
     @property
     def width(self):
         return self.__width
@@ -69,17 +84,3 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         return print("Bye rectangle...")
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return Rectangle(size, size)
