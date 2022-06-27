d2202dcf6c544b64832cba02dcbd91f0835baa08
Mobius90 <mikiyasdaniel1@gmail.com>
2022-06-27 14:04:59 +0300

    more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+a module that defines a Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')        
