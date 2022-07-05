dad9eee1297ce6c65cfb0776d0bafd1c06eff9be
Mobius90 <mikiyasdaniel1@gmail.com>
2022-07-05 12:31:06 +0300

    input-output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a JSON converter function"""
+
 import json
 
 
 def from_json_string(my_str):
-    """A function that returns an object (Python data structure) represented by a JSON string"""
-    return json.loads(my_str)
+    """A function that returns an object (Python data struct) represented by a JSON string"""
+    return json.loads(my_str))
