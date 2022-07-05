3d5faf8250cd0bd95dc9f6efec5f523cfcad4b7f
Mobius90 <mikiyasdaniel1@gmail.com>
2022-07-05 16:14:01 +0300

    input-output

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     nlist = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -7,6 +8,10 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if i > 0 and nlist[roman_string[i]] > nlist[roman_string[i - 1]]:
             a += nlist[roman_string[i]] - 2 * nlist[roman_string[i - 1]]
+            print(nlist[roman_string])
+            print('a')
         else:
             a += nlist[roman_string[i]]
     return a
+
+print(roman_to_int('IV'))
