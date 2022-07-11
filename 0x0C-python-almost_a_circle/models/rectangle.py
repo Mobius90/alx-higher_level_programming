bef672936ff4d61918a044a8367400bd4316f87d
Mobius90 <mikiyasdaniel1@gmail.com>
2022-07-11 18:22:15 +0300

    aoc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Defines module of Rectangle class"""
 
-
 from models.base import Base
 
 class Rectangle(Base):
     """a rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
