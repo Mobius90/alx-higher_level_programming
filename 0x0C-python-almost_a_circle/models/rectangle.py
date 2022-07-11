3dd18e2cab85d49f578406396ff40c65d71d9d1a
Mobius90 <mikiyasdaniel1@gmail.com>
2022-07-11 18:42:56 +0300

    aoc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines module of Rectangle class"""
 
+
 from models.base import Base
 
 class Rectangle(Base):
@@ -12,3 +13,19 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
